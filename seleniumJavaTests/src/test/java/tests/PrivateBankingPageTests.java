894c33c91c9ba87b61fbb92a89a2c447f788132f
witoldd <w.dziuda@systemycallcenter.pl>
2019-07-20 12:59:33 +0200

    add naviagation to privateBanking site, refactored name of navigation class

--- seleniumJavaTests/src/test/java/tests/PrivateBankingPageTests.java
+++ seleniumJavaTests/src/test/java/tests/PrivateBankingPageTests.java
@@ -1,4 +1,16 @@
 package tests;
 
+import navigation.PageNavigation;
+import org.testng.annotations.Test;
+import pages.PrivateBankingPageObject;
+import urls.AppURLs;
+
 public class PrivateBankingPageTests extends TestFoundations {
+
+    @Test
+    public void privateBankingTests(){
+        PrivateBankingPageObject privateBankingPageObject = new PrivateBankingPageObject(driver);
+        PageNavigation pageNavigation = new PageNavigation(driver);
+        pageNavigation.navigateToPage(AppURLs.getPrivetBankingPage());
+    }
 }
